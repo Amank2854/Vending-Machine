d3f27316aeda656b42322f04da8502e8fe74d96f
OptimalKnight <84573266+OptimalKnight@users.noreply.github.com>
2021-11-16 17:35:17 +0530

    Minor fixes and added comments

--- VendingMachine.v
+++ VendingMachine.v
@@ -1,14 +1,16 @@
+//module for the first item
 module Item_One(nickel_in, dime_in, clock, reset, nickel_out, dispense);
+    //defining the inputs and the outputs
     input nickel_in, dime_in, clock, reset;
     output reg nickel_out, dispense;
-    
     reg [4:0] current_state, next_state;
-    localparam  S0  = 7'b00001;
-                S5  = 7'b00010;
-                S10 = 7'b00100;
-                S15 = 7'b01000;
-                S20 = 7'b10000;
-    
+    //defining the states
+    localparam  S0  = 5'b00001,
+                S5  = 5'b00010,
+                S10 = 5'b00100,
+                S15 = 5'b01000,
+                S20 = 5'b10000;
+    //the state will change when there is a positive edge in either clock or reset
     always @(posedge clock or reset) begin
         if(reset) begin
             current_state <= S0;
@@ -17,7 +19,7 @@ module Item_One(nickel_in, dime_in, clock, reset, nickel_out, dispense);
             current_state <= next_state;
         end
     end
-
+    //defining the relationships among the states
     always @(nickel_in or dime_in) begin
         case(current_state)
             S0: 
@@ -36,30 +38,30 @@ module Item_One(nickel_in, dime_in, clock, reset, nickel_out, dispense);
                 else               begin next_state = S10; {nickel_out, dispense} = 2'b00; end
             
             S15:
-                next_state = S0;
+                begin next_state = S0; {nickel_out, dispense} = 2'b00; end
             
             S20:
-                next_state = S0;
+                begin next_state = S0; {nickel_out, dispense} = 2'b00; end
 
             default:
                 begin next_state = S0; {nickel_out, dispense} = 2'b00; end
             
             endcase
     end
-
+//end of the module
 endmodule
-
+//module for the second item
 module Item_Two(nickel_in, dime_in, clock, reset, nickel_out, dispense);
     input nickel_in, dime_in, clock, reset;
     output reg nickel_out, dispense;
 
     reg [5:0] current_state, next_state;
-    localparam  S0  = 7'b000001;
-                S5  = 7'b000010;
-                S10 = 7'b000100;
-                S15 = 7'b001000;
-                S20 = 7'b010000;
-                S25 = 7'b100000;
+    localparam  S0  = 6'b000001,
+                S5  = 6'b000010,
+                S10 = 6'b000100,
+                S15 = 6'b001000,
+                S20 = 6'b010000,
+                S25 = 6'b100000;
     
     always @(posedge clock or reset) begin
         if(reset) begin
@@ -93,10 +95,10 @@ module Item_Two(nickel_in, dime_in, clock, reset, nickel_out, dispense);
                 else               begin next_state = S15; {nickel_out, dispense} = 2'b00; end
             
             S20:
-                next_state = S0;
+                begin next_state = S0; {nickel_out, dispense} = 2'b00; end
             
             S25:
-                next_state = S0;
+                begin next_state = S0; {nickel_out, dispense} = 2'b00; end
 
             default:
                 begin next_state = S0; {nickel_out, dispense} = 2'b00; end
@@ -105,18 +107,18 @@ module Item_Two(nickel_in, dime_in, clock, reset, nickel_out, dispense);
     end
 
 endmodule
-
+//module for the third item
 module Item_Three(nickel_in, dime_in, clock, reset, nickel_out, dispense);
     input nickel_in, dime_in, clock, reset;
     output reg nickel_out, dispense;
     
     reg [6:0] current_state, next_state;
-    localparam  S0  = 7'b0000001;
-                S5  = 7'b0000010;
-                S10 = 7'b0000100;
-                S15 = 7'b0001000;
-                S20 = 7'b0010000;
-                S25 = 7'b0100000;
+    localparam  S0  = 7'b0000001,
+                S5  = 7'b0000010,
+                S10 = 7'b0000100,
+                S15 = 7'b0001000,
+                S20 = 7'b0010000,
+                S25 = 7'b0100000,
                 S30 = 7'b1000000;
     
     always @(posedge clock or reset) begin
@@ -156,10 +158,10 @@ module Item_Three(nickel_in, dime_in, clock, reset, nickel_out, dispense);
                 else               begin next_state = S20; {nickel_out, dispense} = 2'b00; end
             
             S25:
-                next_state = S0;
+                begin next_state = S0; {nickel_out, dispense} = 2'b00; end
             
             S30:
-                next_state = S0;
+                begin next_state = S0; {nickel_out, dispense} = 2'b00; end
 
             default:
                 begin next_state = S0; {nickel_out, dispense} = 2'b00; end
