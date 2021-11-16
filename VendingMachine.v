14d6cb1d9b116a00856916003854ebaa8252519d
Amank2854 <75082110+Amank2854@users.noreply.github.com>
2021-11-16 18:07:02 +0530

    Completed the Source Code.

--- VendingMachine.v
+++ VendingMachine.v
@@ -170,3 +170,105 @@ module Item_Three(nickel_in, dime_in, clock, reset, nickel_out, dispense);
     end
 
 endmodule
+//module for the fourth item
+module Item_Four(nickel_in, dime_in, clock, reset, nickel_out, dispense);
+    input nickel_in, dime_in, clock, reset;
+    output reg nickel_out, dispense;
+    
+    reg [7:0] current_state, next_state;
+    localparam  S0  = 8'b00000001,
+                S5  = 8'b00000010,
+                S10 = 8'b00000100,
+                S15 = 8'b00001000,
+                S20 = 8'b00010000,
+                S25 = 8'b00100000,
+                S30 = 8'b01000000,
+                S35 = 8'b10000000;
+    
+    always @(posedge clock or reset) begin
+        if(reset) begin
+            current_state <= S0;
+        end
+        else begin
+            current_state <= next_state;
+        end
+    end
+
+    always @(nickel_in or dime_in) begin
+        case(current_state)
+            S0: 
+                if(nickel_in)      begin next_state = S5;  {nickel_out, dispense} = 2'b00; end
+                else if(dime_in)   begin next_state = S10; {nickel_out, dispense} = 2'b00; end
+                else               begin next_state = S0;  {nickel_out, dispense} = 2'b00; end
+            
+            S5: 
+                if(nickel_in)      begin next_state = S10; {nickel_out, dispense} = 2'b00; end
+                else if(dime_in)   begin next_state = S15; {nickel_out, dispense} = 2'b00; end
+                else               begin next_state = S5;  {nickel_out, dispense} = 2'b00; end
+            
+            S10:
+                if(nickel_in)      begin next_state = S15; {nickel_out, dispense} = 2'b00; end
+                else if(dime_in)   begin next_state = S20; {nickel_out, dispense} = 2'b00; end
+                else               begin next_state = S10; {nickel_out, dispense} = 2'b00; end
+            
+            S15:
+                if(nickel_in)      begin next_state = S20; {nickel_out, dispense} = 2'b00; end
+                else if(dime_in)   begin next_state = S25; {nickel_out, dispense} = 2'b01; end
+                else               begin next_state = S15; {nickel_out, dispense} = 2'b00; end
+            
+            S20:
+                if(nickel_in)      begin next_state = S25; {nickel_out, dispense} = 2'b00; end
+                else if(dime_in)   begin next_state = S30; {nickel_out, dispense} = 2'b01; end
+                else               begin next_state = S20; {nickel_out, dispense} = 2'b00; end
+            
+            S25:
+                if(nickel_in)      begin next_state = S30; {nickel_out, dispense} = 2'b01; end
+                else if(dime_in)   begin next_state = S35; {nickel_out, dispense} = 2'b11; end
+                else               begin next_state = S25; {nickel_out, dispense} = 2'b00; end
+            
+            S30:
+                begin next_state = S0; {nickel_out, dispense} = 2'b00; end
+
+            S35:
+                begin next_state = S0; {nickel_out, dispense} = 2'b00; end
+
+            default:
+                begin next_state = S0; {nickel_out, dispense} = 2'b00; end
+            
+            endcase
+    end
+
+endmodule
+//the main module
+module VendingMachine(item_number, nickel_in, dime_in, clock, reset, nickel_out, dispense);
+    //defining the inputs and the outputs
+    input [3:0] item_number;
+    input nickel_in, dime_in, clock, reset;
+    output reg nickel_out, dispense;
+  	reg No1, No2, No3, No4, D1, D2, D3, D4;
+    //calling the modules for all items
+    Item_One   IO(.nickel_in(nickel_in), .dime_in(dime_in), .clock(clock), .reset(reset), .nickel_out(No1), .dispense(D1));
+    Item_Two   ITW(.nickel_in(nickel_in), .dime_in(dime_in), .clock(clock), .reset(reset), .nickel_out(No2), .dispense(D2));
+    Item_Three ITH(.nickel_in(nickel_in), .dime_in(dime_in), .clock(clock), .reset(reset), .nickel_out(No3), .dispense(D3));
+    Item_Four  IF(.nickel_in(nickel_in), .dime_in(dime_in), .clock(clock), .reset(reset), .nickel_out(No4), .dispense(D4));
+    //updating the output values with the required module values
+    always @(*) begin
+        if(item_number == 4'b0001) begin
+            nickel_out = No1;
+            dispense = D1;
+            end
+        else if(item_number == 4'b0010) begin
+            nickel_out = No2;
+            dispense = D2;
+            end
+        else if(item_number == 4'b0100) begin
+            nickel_out = No3;
+            dispense = D3;
+            end
+        else if(item_number == 4'b1000) begin
+            nickel_out = No4;
+            dispense = D4;
+        end
+    end
+
+endmodule
